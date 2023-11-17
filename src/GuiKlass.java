e3c8bae1360bc0cec78ed14220c3ab089a521751
Anna Boronkai <annaboronki@hotmail.com>
2023-11-17 15:28:35 +0100

    change

--- src/GuiKlass.java
+++ src/GuiKlass.java
@@ -8,21 +8,56 @@ public class GuiKlass extends JFrame {
     JTextArea userName = new JTextArea();
     JButton enterNameButton = new JButton("Klart");
     JButton startButton = new JButton("Starta nytt spel");
-    JPanel panel = new JPanel();
+    JPanel panelName = new JPanel();
+
+
+
+
+    JPanel panelGameMenu = new JPanel();
+    JButton randomPlayerButton = new JButton("Slumpad spelare");
+    JButton playAgainstAFriendButton = new JButton("Spela mot en vän");
+
+
     public GuiKlass(){
 
-        this.add(panel);
-        panel.setLayout(new GridLayout(3,1));
+       getNameWindow();
+       // getGameMenu();
 
-        panel.add(namePromt);
-        panel.add(userName);
-        panel.add(enterNameButton);
+
+    }
+
+    public void getNameWindow(){
+        this.add(panelName);
+        panelName.setLayout(new GridLayout(3,1));
+
+        panelName.add(namePromt);
+        panelName.add(userName);
+        panelName.add(enterNameButton);
         enterNameButton.addActionListener(new GameActionListener());
 
         setVisible(true);
-        setSize(100, 200);
+        setSize(200, 200);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void getGameMenu(){
+        this.add(panelGameMenu);
+        panelGameMenu.setLayout(new GridLayout(1,2));
+        add(randomPlayerButton);
+        randomPlayerButton.setPreferredSize(new Dimension(150, 100));
+        add(playAgainstAFriendButton);
+        playAgainstAFriendButton.setPreferredSize(new Dimension(150, 100));
+
+        setVisible(true);
+
+        setSize(100, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void getCategories(){
+
 
     }
 }
