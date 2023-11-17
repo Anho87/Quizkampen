dd02e5f0505f774d771445e6d8b33a3c17afcbb0
Anna Boronkai <annaboronki@hotmail.com>
2023-11-17 11:10:20 +0100

    initila commit

---
+++ src/Quizkampen/GuiClass.java
@@ -0,0 +1,18 @@
+package Quizkampen;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GuiClass extends JFrame implements ActionListener {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
