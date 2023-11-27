38370234b26a975ef697d4f325a1c4a23e135919
Anna Boronkai <annaboronki@hotmail.com>
2023-11-27 12:05:13 +0100

    added settings class

--- src/Settings.java
+++ src/Settings.java
@@ -1,20 +1,29 @@
+import java.io.FileInputStream;
 import java.util.Properties;
 
 public class Settings {
 
-    int round;
+    int rounds;
     int questions;
 
     public Settings(){
         Properties properties = new Properties();
         try {
-            properties.load();
+            properties.load(new FileInputStream("prop.properties"));
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            System.out.println("Filen kunde inte hittas");
+            rounds = 2;
+            questions = 2;
         }
-
-
+        rounds = Integer.parseInt(properties.getProperty("round", "2"));
+        questions = Integer.parseInt(properties.getProperty("questions", "2"));
     }
 
+    public int getRounds() {
+        return rounds;
+    }
 
+    public int getQuestions() {
+        return questions;
+    }
 }
